84e97a77138bc77d489ba3211f2dfcba18d8ef30
Mart-Vosumagi <mvosumagi@gmail.com>
2025-09-19 10:20:26 +0300

    index pangast

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,11 @@
 <template>
 
   <nav>
-    <router-link to="/">Kodu</router-link> |
-    <router-link to="/atms">Pangaautomaadid</router-link> |
+    <router-link to="/home">Home</router-link> |
+    <router-link to="/login">Log in</router-link> |
+    <router-link to="/user">User</router-link> |
 
-    <template v-if="isAdmin">
-      <router-link to="/location">Asukoht</router-link> |
-    </template>
 
-    <template v-if="isLoggedIn">
-      <font-awesome-icon @click="startLogOut" class="cursor-pointer" icon="fa-solid fa-right-from-bracket"/>
-    </template>
-
-    <template v-if="!isLoggedIn">
-      <router-link to="/login">Sisse logimine</router-link>
-    </template>
 
   </nav>
   <router-view @event-user-logged-in="updateNavMenu"/>
