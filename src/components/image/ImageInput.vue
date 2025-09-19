bde9f0e5ff53b21c2d4bfa441eeab02886730d29
Lauri <fg@hot.ee>
2025-09-19 14:40:33 +0300

    My services and error views

---
+++ src/components/image/ImageInput.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="mb-3">
+    <input ref="fileInput" class="form-control" type="file" @change="handleImage" accept="image/x-png,image/jpeg,image/gif">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ImageInput',
+  props: {
+    resetFileInput: Boolean
+  },
+  watch: {
+    resetFileInput(newValue) {
+      if (newValue) {
+        this.clearFileInput()
+      }
+    },
+  },
+  methods: {
+    handleImage(event) {
+      const selectedImage = event.target.files[0];
+      this.emitBase64(selectedImage);
+    },
+
+    emitBase64(fileObject) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        let imageDataBase64 = reader.result;
+        this.$emit('event-new-image-selected', imageDataBase64)
+      };
+      reader.onerror = function (error) {
+        alert(error);
+      }
+      reader.readAsDataURL(fileObject);
+    },
+
+    clearFileInput() {
+      if (this.$refs.fileInput) {
+        this.$refs.fileInput.value = ''
+        this.$emit('event-reset-image-select-complete')
+      }
+    },
+
+  }
+}
+</script>
--- src/views/ServiceView.vue
+++ src/views/ServiceView.vue
@@ -1,11 +1,109 @@
-<script setup>
-
-</script>
 
 <template>
+  <div class="container text-center">
+    <div class="row">
+      <div class="col">
+        <h1>Your Services</h1>
+      </div>
+    </div>
 
+
+    <div class="row justify-content-center">
+      <div class="col col-3">
+        <ServicesDropdown :cities="services" :selected-service-id="selectedServiceId"
+                          @event-new-service-selected="handleSelectedServiceChange"
+        />
+      </div>
+      <div class="col col-6">
+        <AlertDanger :message="errorMessage"/>
+        <AlertSuccess :message="successMessage"/>
+
+        />
+      </div>
+
+      <div class="mb-3">
+        <label class="form-label">Service name</label>
+        <input v-model="servicename" type="email" class="form-control">
+      </div>
+
+      <div class="mb-3">
+        <label class="form-label">Detailed description</label>
+        <input v-model="detailedDescription" type="email" class="form-control">
+      </div>
+
+      <div class="mb-3">
+        <label class="form-label">Unit cost</label>
+        <input v-model="unitPrice " type="email" class="form-control">
+      </div>
+
+
+      <button @click=" " type="submit" class="btn btn-outline-primary">Back</button>
+
+
+    </div>
+  </div>
 </template>
+
 
 <style scoped>
 
 </style>
+
+
+<script>
+import ServicesDropdown from "@/components/modal/ServicesDropdown.vue";
+import AlertDanger from "@/components/alert/AlertDanger.vue";
+import AlertSuccess from "@/components/alert/AlertSuccess.vue";
+
+export default {
+  name: 'ServiceView',
+  components: {AlertSuccess, AlertDanger, ServicesDropdown},
+  data() {
+    return {
+      userId: Number(sessionStorage.getItem('userId')),
+      roleName: sessionStorage.getItem('roleName'),
+      selectedServiceId: 0,
+      errorMessage: '',
+      successMessage: '',
+
+
+      services: [
+        {
+          serviceId: 0,
+          serviceName: ''
+        }
+      ],
+
+
+      errorResponse: {
+        message: '',
+        errorCode: 0
+      }
+
+    }
+  },
+  methods: {
+
+    getServices() {
+      ServiceService.sendGetServicesRequest()
+          .then(response => this.services = response.data)
+          .catch(() => NavigationService.navigateToErrorView())
+    },
+
+
+    resetErrorMessage() {
+      this.errorMessage = ''
+    },
+
+    resetSuccessMessage() {
+      this.successMessage = ''
+    },
+
+
+  },
+  mounted() {
+    this.getServices()
+
+  }
+}
+</script>
