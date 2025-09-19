d2419ca532be40d5cfe7df0b9755a244f6c97402
Mart-Vosumagi <mvosumagi@gmail.com>
2025-09-19 09:35:53 +0300

    index

--- src/router/index.js
+++ src/router/index.js
@@ -1,72 +1,75 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import ErrorView from "@/views/ErrorView.vue";
+import ServiceView from "@/views/ServiceView.vue";
+import UserView from "@/views/UserView.vue";
+import LoginView from "@/views/LoginView.vue";
+import MyOrdersView from "@/views/MyOrdersView.vue";
+import MyServicesView from "@/views/MyServicesView.vue";
+
 
 const routes = [
-  {
-    path: '/home',
-    name: 'homeRoute',
-    component: HomeView
-  },
+    {
+        path: '/home',
+        name: 'homeRoute',
+        component: HomeView
+    },
 
-  {
-    path: '/error',
-    name: 'errorRoute',
-    component: ErrorView
-  },
-  {
-    path: '/service',
-    name: 'serviceRoute',
-    component: ServiceView
-  },
-  {
-    path: '/login',
-    name: 'loginRoute',
-    component: LoginView
-  },
-  {
-    path: '/user',
-    name: 'userRoute',
-    component: UserView
-  },
-  {
-    path: '/myorders',
-    name: 'myOrdersRoute',
-    component: MyOrdersView
-  },
+    {
+        path: '/error',
+        name: 'errorRoute',
+        component: ErrorView
+    },
+    {
+        path: '/service',
+        name: 'serviceRoute',
+        component: ServiceView
+    },
+    {
+        path: '/login',
+        name: 'loginRoute',
+        component: LoginView
+    },
+    {
+        path: '/user',
+        name: 'userRoute',
+        component: UserView
+    },
+    {
+        path: '/myorders',
+        name: 'myOrdersRoute',
+        component: MyOrdersView
+    },
 
-  {
-    path: '/myservices',
-    name: 'myServicesRoute',
-    component: MyServicesView
-  },
-
-
+    {
+        path: '/myservices',
+        name: 'myServicesRoute',
+        component: MyServicesView
+    },
 
 
-
-  {
-    path: '/not-authorized',
-    name: 'notAuthorizedRoute',
-    component: NotAuthorizedView
-  },
-
+    {
+        path: '/not-authorized',
+        name: 'notAuthorizedRoute',
+        component: NotAuthorizedView
+    },
 
 
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: function () {
-      return import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    {
+        path: '/about',
+        name: 'about',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: function () {
+            return import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+        }
     }
-  }
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+    history: createWebHistory(process.env.BASE_URL),
+    routes
 })
 
 export default router
