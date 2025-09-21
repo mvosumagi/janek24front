e770a9e95771e972acebb3dd32095037565fa0f5
Mart-Vosumagi <mvosumagi@gmail.com>
2025-09-21 15:16:22 +0300

    company chechkbox not working

--- src/components/user/CompanyCheckbox.vue
+++ src/components/user/CompanyCheckbox.vue
@@ -1,23 +1,23 @@
 <template>
-  <div class="form-floating mb-3">
-    <input :value="iscompany" @change="handleIsCompanyCheck" type="checkbox" class="form-check-input"/>
-    <label>Company</label>
-  </div>
+    <div class="form-check mb-3">
+      <input :checked="isCompany" @change="handleIsCompanyCheck" type="checkbox" class="form-check-input"/>
+      <label>Company</label>
+    </div>
 
-</template>
+  </template>
 
-<script>
-export default {
-  name: "CompanyCheckbox",
-  props: {
-    iscompany: Boolean
-  },
-  methods: {
-    handleIsCompanyCheck(event) {
-      this.$emit("event-is-company-updated", event.target.checked);
+  <script>
+  export default {
+    name: "CompanyCheckbox",
+    props: {
+      isCompany: Boolean
+    },
+    methods: {
+      handleIsCompanyCheck(event) {
+        this.$emit("event-is-company-updated", event.target.checked);
+      }
     }
+
+
   }
-
-
-}
-</script>
+  </script>
