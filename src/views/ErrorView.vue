0af568d22bf6124041d260b9a6f6b43b2b5ac6a2
Mart-Vosumagi <mvosumagi@gmail.com>
2025-09-22 13:30:55 +0300

    login complete

--- src/views/ErrorView.vue
+++ src/views/ErrorView.vue
@@ -1,10 +1,25 @@
-<script setup>
-
-</script>
-
 <template>
+  <div class="container text-center">
+    <div class="row">
+      <div class="col">
+        Kontrolli! Midagi läks valesti! :)
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <img src="@/assets/error.jpg" alt="plahvatus" class="img-fluid">
+      </div>
+    </div>
+  </div>
+
 
 </template>
+<script>
+export default {
+  name: 'ErrorView'
+}
+</script>
+
 
 <style scoped>
 
