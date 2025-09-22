508ed687f641a34f09b655c383a9969a4692b239
Lauri <fg@hot.ee>
2025-09-22 14:21:21 +0300

    My services and error views

--- src/views/MyServicesView.vue
+++ src/views/MyServicesView.vue
@@ -2,7 +2,16 @@
 
 </script>
 
-<template>
+
+
+  <template>
+    <div class="container text-center">
+      <div class="row">
+        <div class="col">
+          <h1>My Services</h1>
+        </div>
+      </div>
+    </div>
 
 </template>
 
