c2ae164c666ccc921dd3034b9ad87bd61843049a
opilane <seemned@mail.ee>
2025-09-29 15:02:31 +0300

    ajurünnak

--- src/views/OrderingView.vue
+++ src/views/OrderingView.vue
@@ -1,4 +1,8 @@
-<script setup>
+<input type="text" id="calendar" />
+<script src="https://cdn.jsdelivr.net/npm/flatpickr">
+
+
+flatpickr("#calendar", {});
 </script>
 <template>
   <img src="/Rain.jpg" alt="Public Image"/>
